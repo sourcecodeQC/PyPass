0a29e7cdfc169faefac00c4aa4b714f080333620
sourcecodeQC <alek.de.mehlem@gmail.com>
2023-06-14 09:24:38 +0200

    creation of repo; pseudo code

---
+++ main.py
@@ -0,0 +1,16 @@
+import string
+import random
+
+
+# Gettin' passwd lenght
+lenght = int(input("Enter password lenght: "))
+
+print('''Choose character set for password from these:
+         1. Digits
+         2. Letters
+         3. Special characters
+         4. Exit''')
+
+characterList = ""
+
+
